d5d1c42975dfd80ea064fe41b403f2757cab4e42
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2024-03-17 18:49:09 +0800

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="text-xl font-semibold">Let's see the games</h1>
-      <Link href="/games">Games list</Link>
+      <h1 className="text-xl font-semibold">
+        To see the games click the link below
+      </h1>
+      <Link
+        href="/games"
+        className="text-sm font-semibold leading-6 text-indigo-600"
+        aria-describedby="featured-post"
+      >
+        Games list <span aria-hidden="true">&rarr;</span>
+      </Link>
     </>
   );
 }
